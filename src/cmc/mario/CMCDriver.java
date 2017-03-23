df42cdb391a0011a51c9d8d35de3343110c5cfb2
j1thao <j1thao@viewlincsci02.ad.csbsju.edu>
2017-03-23 00:14:02 -0500

    RECOMMENDATION UPDATED:  NOT RUNNING

--- src/cmc/mario/CMCDriver.java
+++ src/cmc/mario/CMCDriver.java
@@ -67,16 +67,47 @@ public class CMCDriver {
 		
 		
 		//Recommendation --Jing
+		System.out.println("START OF RECOMMENDATION CLASS");
+		List<University> uniList = new ArrayList<University>();
+		List<String> popMajors = new ArrayList<String>();
+		SearchController seCon = new SearchController();
+		String m1 = "Biology";
+		String m2 = "Computer Science";
+		popMajors.add(m1);
+		popMajors.add(m2);
+		University bitch = new University("ABILENE CHRISTIAN UNIVERSITY","TEXAS","SUBURBAN","PRIVATE",
+				10000, 50 , -1,-1 ,12088, 70 ,4000,90 , 80 , 2,3,3,popMajors);
+		University bitch1 = new University("AUBURN","NEW YORK","SMALL-CITY","STATE",35000
+				,21,480,545,12468,50,5500,90,60,2,4,4,popMajors);
+		University bitch2 = new University("ARIZONA STATE","ARIZONA","-1","STATE",40000
+				,59,450,500,16181,50,17000,80,60,3,4,5, popMajors);
+		University bitch3 = new University("BARD","NEW YORK","SMALL-CITY","PRIVATE",10000,55
+				,560,520,32239,80,4000,70,30,3,4,4,popMajors);
+		University bitch4 = new University("AUGSBURG","MINNESOTA","SMALL-CITY","PRIVATE",10000
+				,43,420,490,29991,80,4000,85,50,1,3,4,popMajors);
+		University bitch5 = new University("BARNARD","NEW YORK","URBAN","PRIVATE",10000
+				,100,630,610,31009,60,4000,60,20,4,3,4,popMajors);
+		University bitch6 = new University("BAYLOR UNIVERSITY", "TEXAS","URBAN","PRIVATE",15000
+				,55,485,521,21658,65,11500,65,75,4,4,3,popMajors);
+		uniList.add(bitch);
+		uniList.add(bitch1);
+		uniList.add(bitch2);
+		uniList.add(bitch3);
+		uniList.add(bitch4);
+		uniList.add(bitch5);
+		uniList.add(bitch6);
+		seCon.viewRecommendation(bitch);
+		System.out.println("END OF RECOMMENDATION CLASS");
 		
 		
 		
 		
 		
-		//Search and Account UI - Kalila
-		//U5: Search for Schools
-		//U6: View Search Results
-		//U7: View Specific School
-		//U21: Logout
+//		Search and Account UI - Kalila
+//		U5: Search for Schools
+//		U6: View Search Results
+//		U7: View Specific School
+//		U21: Logout
 		List<String> popMajor = new ArrayList<String>(5);
 		String s1 = "Biology";
 		String s2 = "Art-history";

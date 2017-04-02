56f41b4a9962a4caef615200dca224b5afe425d7
s1vazquez <s1vazquez@viewlincsci25.ad.csbsju.edu>
2017-04-02 14:00:37 -0500

    user constructors

--- Testing/cmc/mario/entities/UserTest.java
+++ Testing/cmc/mario/entities/UserTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
-private User user;
+private User user,user2;
 
 	@Before
 	  public void setUp() throws Exception {
@@ -23,6 +23,52 @@ private User user;
 		assertEquals("Password is " + expResult,expResult, result);
 	}
 
+	@Test
+	public void testDefaultConstructor(){
+		user = new User();
+	    user.setFirstName("jing");
+	    user.setLastName("thao");
+	    user.setUsername("j1thao");
+	    user.setPassword("look!@you");
+	    user.setTypeOfUser('u');
+	    String expResult = "jing";
+	    String expResult2 = "thao";
+	    String expResult3 = "j1thao";
+	    String expResult4 = "look!@you";
+	    char expResult5 = 'u';
+		String result = user.getFirstName();
+		String result2 = user.getLastName();
+		String result3 = user.getUserName();
+		String result4 = user.getPassword();
+		String result5 = user.getType();
+		assertEquals("firstname is " + expResult,expResult, result);
+		assertEquals("lastname is " + expResult2,expResult2, result2);
+		assertEquals("username is " + expResult3,expResult3, result3);
+		assertEquals("password is " + expResult4,expResult4, result4);
+		assertEquals("type is " + expResult5,expResult5, result5);
+	    
+	}
+	
+	@Test
+	public void testSecondConstructor(){
+		 user2 = new User("kalila","Moua", "k1moua", "k3$ha")
+		 String expResult = "kalila";
+	     String expResult2 = "Moua";
+	     String expResult3 = "k1moua";
+    	 String expResult4 = "k3$ha";
+		 char expResult5 = 'u';
+		 String result = user2.getFirstName();
+		 String result2 = user2.getLastName();
+		 String result3 = user2.getUserName();
+		 String result4 = user2.getPassword();
+		 String result5 = user2.getType();
+		 assertEquals("firstname is " + expResult,expResult, result);
+		 assertEquals("lastname is " + expResult2,expResult2, result2);
+		 assertEquals("username is " + expResult3,expResult3, result3);
+		 assertEquals("password is " + expResult4,expResult4, result4);
+		 assertEquals("type is " + expResult5,expResult5, result5);
+	}
+	
 	@Test
 	public void testGetFirstName() {
 		String expResult = "tree";
